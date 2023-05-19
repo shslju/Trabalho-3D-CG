634550b7d0996a8160411de0d4bb2763c4f12f63
shslju <shsljuu@gmail.com>
2023-05-19 15:51:06 -0300

    finish scene1

--- scene1.c
+++ scene1.c
@@ -1,32 +1,32 @@
 #include <windows.h>
 #include <stdio.h>
 #include <gl/glut.h>
-int ambient_lighting_enabled = 1;
-int lighting0_enabled = 0;
-float t_y = -1;
+int face_culling = 0;
+GLfloat x_ok = 2.0f, y_ok = 2.0f, z_ok = 2.0f;
 GLfloat angle = 30;
+
 void lighting()
 {
-    float position[4] = {2.0f, 2.0f, 2.0f, 0.0f};
+    float position[4] = {-1, 4, -1, 1.0f};
     float white[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-    glLightfv(GL_LIGHT0, GL_POSITION, position);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+    float dim_white[4] = {0.2f, 0.2f, 0.2f, 1.0f};
     float global_ambient[4] = {0.2f, 0.2f, 0.2f, 1.0f};
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
-    if (ambient_lighting_enabled)
-    {
-        // remoção de superfícies ocultas
-        // glEnable(GL_CULL_FACE);
-        glEnable(GL_LIGHTING);
-        glEnable(GL_LIGHT0);
-    }
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, dim_white);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
     glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+
+    printf("x: %f, y:%f, z:%f\n", x_ok, y_ok, z_ok);
 }
 
 void init()
 {
-    glClearColor(0.60f, 0.60f, 0.60f, 1.0f);
+    glClearColor(0.50f, 0.50f, 0.50f, 1.0f);
     glEnable(GL_DEPTH_TEST);
     lighting();
 }
@@ -38,76 +38,111 @@ void changeVision()
     gluPerspective(angle, 1, 0.1, 500);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(2, 2, 2, 0, 0, 0, 0, 1, 0);
+    gluLookAt(x_ok, y_ok, z_ok, 0, 0, 0, 0, 1, 0);
 }
 
 void GerenciaTeclado(unsigned char key, int x, int y)
 {
-    printf("d");
+    float additive = 0.5f;
     switch (key)
     {
     case 'd':
-        // x_ok++;
+        x_ok = x_ok + additive;
         break;
     case 'a':
-        // x_ok--;
+        x_ok = x_ok - additive;
+        break;
+    case 's':
+        y_ok = y_ok - additive;
         break;
     case 'w':
+        y_ok = y_ok + additive;
+        break;
+    case 'q':
+        z_ok = z_ok + additive;
+        break;
+    case 'e':
+        z_ok = z_ok - additive;
+        break;
+    case 'f':
+        if (face_culling)
+        {
+            face_culling = 0;
+        }
+        else
+        {
+            face_culling = 1;
+        }
+        break;
+    case '=':
         // zoom
         if (angle >= 10)
         {
             angle -= 5;
         }
         break;
-    case 's':
+    case '-':
         // tirar zoom hihi
         if (angle <= 130)
         {
             angle += 5;
         }
         break;
-    case 'k':
-        ambient_lighting_enabled = 1;
-    case 'j':
-        ambient_lighting_enabled = 0;
     default:
         break;
     }
-    // printf("%f %f\n", x_ok, t_y);
     changeVision();
     glutPostRedisplay();
 }
 
 void display()
 {
+    changeVision();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
+    // CULL FACE
+    if (face_culling)
+    {
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+    }
+    else
+    {
+        glEnable(GL_CULL_FACE);
+    }
+    //
+    glFrontFace(GL_CCW);
+    // CUBE LIGHT COEFFICIENTS
     float w_kd[4] = {1.0f, 0.0f, 0.0f, 1.0f};
-    float w_ks[4] = {0.2f, 0.2f, 0.2f, 1.0f};
+    float w_ks[4] = {1.0f, 0.01f, 0.01f, 1.0f};
     float w_ns = 1.0f;
     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, w_kd);
     glMaterialfv(GL_FRONT, GL_SPECULAR, w_ks);
     glMaterialf(GL_FRONT, GL_SHININESS, w_ns);
-    glBegin(GL_QUADS);
-    // chao
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(-1, 0, -1);
-    glVertex3f(1, 0, -1);
-    glVertex3f(1, 0, 1);
-    glVertex3f(-1, 0, 1);
-    // parede rsrs
-    glColor3f(1.0f, 0.0f, 1.0f);
-    glVertex3f(-1, 0, -1);
-    glVertex3f(1, 0, -1);
-    glVertex3f(1, 1, -1);
-    glVertex3f(-1, 1, -1);
-    // parede rsrs
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(-1, 1, 1);
-    glVertex3f(-1, 0, 1);
-    glVertex3f(-1, 0, -1);
-    glVertex3f(-1, 1, -1);
-    glEnd();
+    // PRIMEIRA PAREDE
+    GLfloat translation_cube[3] = {-5.65f, 0.15f, 0.0f};
+    glPushMatrix();
+    glScalef(0.1f, 1.5f, 5.65f);
+    glTranslatef(translation_cube[0], translation_cube[1], translation_cube[2]);
+    glutSolidCube(0.2f);
+    glPopMatrix();
+    // SEGUNDA PAREDE
+    GLfloat translation_cube2[3] = {0.00f, 0.15f, -5.65f};
+    glPushMatrix();
+    glScalef(5.65f, 1.5f, 0.1f);
+    glTranslatef(translation_cube2[0], translation_cube2[1], translation_cube2[2]);
+    glutSolidCube(0.2f);
+    glPopMatrix();
+    // CHAO
+    GLfloat translation_cube3[3] = {0.00f, 0.60f, 0.0f};
+    glPushMatrix();
+    glScalef(5.65f, 0.1f, 5.65f);
+    glTranslatef(translation_cube3[0], translation_cube3[1], translation_cube3[2]);
+    glutSolidCube(0.2f);
+    glPopMatrix();
+
+    // PARTE DA FRENTE DO BULE RENDERIZADO COM INDEXAÇÃO EM DIREÇÃO HORÁRIA
+    glFrontFace(GL_CW);
     // TEAPOT LIGHT COEFFICIENTS
     float kd[4] = {0.0f, 0.65f, 0.80f, 1.0f};
     float ks[4] = {0.9f, 0.9f, 0.9f, 1.0f};
@@ -117,42 +152,49 @@ void display()
     glMaterialf(GL_FRONT, GL_SHININESS, ns);
     glMatrixMode(GL_MODELVIEW);
     // TEAPOT TRANSLATION
-    GLfloat translation_teapot[3] = {-0.25f, 0.15f, 0.40f};
+    GLfloat translation_teapot[3] = {-0.25f, 0.22f, 0.30f};
     glPushMatrix();
     glTranslatef(translation_teapot[0], translation_teapot[1], translation_teapot[2]);
-    glRotatef(25, 0, 1, 0);
+    // glRotatef(50, 0, 1, 0);
     glutSolidTeapot(0.2f);
     glPopMatrix();
+
+    glFrontFace(GL_CCW);
     // SPHERE LIGHT COEFFICIENTS
-    float kd_s[4] = {1.0f, 0.7f, 0.0f, 1.0f};
-    float ks_s[4] = {0.1f, 0.1f, 0.1f, 1.0f};
+    float ka_s[4] = {1.0f, 0.6f, 0.0f, 1.0f};
+    float kd_s[4] = {1.0f, 0.6f, 0.0f, 1.0f};
+    float ks_s[4] = {0.0f, 0.0f, 0.0f, 1.0f};
     float ns_s = 10.0f;
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, kd_s);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ka_s);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, kd_s);
     glMaterialfv(GL_FRONT, GL_SPECULAR, ks_s);
     glMaterialf(GL_FRONT, GL_SHININESS, ns_s);
     glMatrixMode(GL_MODELVIEW);
     // SPHERE TRANSLATION
-    GLfloat translation_sphere[3] = {-0.25f, 0.15f, -0.25f};
+    GLfloat translation_sphere[3] = {-0.25f, 0.20f, -0.25f};
     glPushMatrix();
     glTranslatef(translation_sphere[0], translation_sphere[1], translation_sphere[2]);
     glutSolidSphere(0.15, 40, 40);
     glPopMatrix();
+
     // TORUS LIGHT COEFFICIENTS
     float kd_t[4] = {0.55f, 1.0f, 0.0f, 1.0f};
-    float ks_t[4] = {0.9f, 0.9f, 0.9f, 1.0f};
+    float ks_t[4] = {0.55f, 0.9f, 0.9f, 1.0f};
     float ns_t = 120.0f;
     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, kd_t);
     glMaterialfv(GL_FRONT, GL_SPECULAR, ks_t);
     glMaterialf(GL_FRONT, GL_SHININESS, ns_t);
     glMatrixMode(GL_MODELVIEW);
-    // TORUS TRANSLATION
-    GLfloat translation_torus[3] = {0.40f, 0.15f, 0.40f};
+    // TORUS TRANSLATION + ROTATION
+    GLfloat translation_torus[3] = {0.40f, 0.20f, 0.10f};
     glPushMatrix();
     glTranslatef(translation_torus[0], translation_torus[1], translation_torus[2]);
     glRotatef(90, 1, 0, 0);
     glutSolidTorus(0.06f, 0.2f, 30, 30);
     glPopMatrix();
+    // DRAW PRIMITIVES
     glFlush();
+    lighting();
 }
 int main(int argc, char **argv)
 {
