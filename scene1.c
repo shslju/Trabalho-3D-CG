9534026ee25f36f82c1baf0a2868363089a241a1
shslju <shsljuu@gmail.com>
2023-05-30 16:09:12 -0300

    add comments+fix lighting+init depth buffer 💀

--- scene1.c
+++ scene1.c
@@ -2,36 +2,37 @@
 #include <stdio.h>
 #include <gl/glut.h>
 int face_culling = 0;
-GLfloat x_ok = 2.0f, y_ok = 2.0f, z_ok = 2.0f;
+GLfloat x_ok = 1.5f, y_ok = 1.0f, z_ok = 2.0f;
 GLfloat angle = 30;
 
 void lighting()
 {
+    // Declaração da fonte de luz da cena
     float position[4] = {-1, 4, -1, 1.0f};
     float white[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-    float dim_white[4] = {0.2f, 0.2f, 0.2f, 1.0f};
+    float black[4] = {0.0f, 0.0f, 0.0f, 1.0f};
     float global_ambient[4] = {0.2f, 0.2f, 0.2f, 1.0f};
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
     glLightfv(GL_LIGHT0, GL_POSITION, position);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, dim_white);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, black);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
     glLightfv(GL_LIGHT0, GL_SPECULAR, white);
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-
-    printf("x: %f, y:%f, z:%f\n", x_ok, y_ok, z_ok);
 }
 
 void init()
 {
     glClearColor(0.50f, 0.50f, 0.50f, 1.0f);
+    // Ocultação de faces ocultas
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
     glEnable(GL_DEPTH_TEST);
     lighting();
 }
 
-void changeVision()
+void gerenciaVisao()
 {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -88,31 +89,26 @@ void GerenciaTeclado(unsigned char key, int x, int y)
             angle += 5;
         }
         break;
+    case 27:
+        exit(0);
+        break;
     default:
         break;
     }
-    changeVision();
+    gerenciaVisao();
+    lighting();
     glutPostRedisplay();
 }
 
 void display()
 {
-    changeVision();
+    gerenciaVisao();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
-    // CULL FACE
-    if (face_culling)
-    {
-        glEnable(GL_CULL_FACE);
-        glCullFace(GL_BACK);
-    }
-    else
-    {
-        glEnable(GL_CULL_FACE);
-    }
-    //
+    lighting();
+    // Essa parte é explicada mais a frente
     glFrontFace(GL_CCW);
-    // CUBE LIGHT COEFFICIENTS
+    // Coeficientes de luz das paredes / chão
     float w_kd[4] = {1.0f, 0.0f, 0.0f, 1.0f};
     float w_ks[4] = {1.0f, 0.01f, 0.01f, 1.0f};
     float w_ns = 1.0f;
@@ -140,10 +136,11 @@ void display()
     glTranslatef(translation_cube3[0], translation_cube3[1], translation_cube3[2]);
     glutSolidCube(0.2f);
     glPopMatrix();
-
-    // PARTE DA FRENTE DO BULE RENDERIZADO COM INDEXAÇÃO EM DIREÇÃO HORÁRIA
+    // Parte da frente do bule foi indexada em sentido horário
+    // então face o culling não funciona normalmente, porque o algoritmo assume
+    // que os vértices são indexados em sentido anti-horário.
     glFrontFace(GL_CW);
-    // TEAPOT LIGHT COEFFICIENTS
+    // Coeficientes de luz do bule
     float kd[4] = {0.0f, 0.65f, 0.80f, 1.0f};
     float ks[4] = {0.9f, 0.9f, 0.9f, 1.0f};
     float ns = 120.0f;
@@ -151,7 +148,7 @@ void display()
     glMaterialfv(GL_FRONT, GL_SPECULAR, ks);
     glMaterialf(GL_FRONT, GL_SHININESS, ns);
     glMatrixMode(GL_MODELVIEW);
-    // TEAPOT TRANSLATION
+    // Translação do bule
     GLfloat translation_teapot[3] = {-0.25f, 0.22f, 0.30f};
     glPushMatrix();
     glTranslatef(translation_teapot[0], translation_teapot[1], translation_teapot[2]);
@@ -160,7 +157,7 @@ void display()
     glPopMatrix();
 
     glFrontFace(GL_CCW);
-    // SPHERE LIGHT COEFFICIENTS
+    // Coeficientes de luz da esfera
     float ka_s[4] = {1.0f, 0.6f, 0.0f, 1.0f};
     float kd_s[4] = {1.0f, 0.6f, 0.0f, 1.0f};
     float ks_s[4] = {0.0f, 0.0f, 0.0f, 1.0f};
@@ -170,14 +167,14 @@ void display()
     glMaterialfv(GL_FRONT, GL_SPECULAR, ks_s);
     glMaterialf(GL_FRONT, GL_SHININESS, ns_s);
     glMatrixMode(GL_MODELVIEW);
-    // SPHERE TRANSLATION
+    // Translação da esfera
     GLfloat translation_sphere[3] = {-0.25f, 0.20f, -0.25f};
     glPushMatrix();
     glTranslatef(translation_sphere[0], translation_sphere[1], translation_sphere[2]);
     glutSolidSphere(0.15, 40, 40);
     glPopMatrix();
 
-    // TORUS LIGHT COEFFICIENTS
+    // Coeficientes de luz do donut
     float kd_t[4] = {0.55f, 1.0f, 0.0f, 1.0f};
     float ks_t[4] = {0.55f, 0.9f, 0.9f, 1.0f};
     float ns_t = 120.0f;
@@ -185,22 +182,21 @@ void display()
     glMaterialfv(GL_FRONT, GL_SPECULAR, ks_t);
     glMaterialf(GL_FRONT, GL_SHININESS, ns_t);
     glMatrixMode(GL_MODELVIEW);
-    // TORUS TRANSLATION + ROTATION
+    // Translação e rotação do donut
     GLfloat translation_torus[3] = {0.40f, 0.20f, 0.10f};
     glPushMatrix();
     glTranslatef(translation_torus[0], translation_torus[1], translation_torus[2]);
     glRotatef(90, 1, 0, 0);
     glutSolidTorus(0.06f, 0.2f, 30, 30);
     glPopMatrix();
-    // DRAW PRIMITIVES
+    // Desenha tudo
     glFlush();
-    lighting();
 }
 int main(int argc, char **argv)
 {
     printf("aa");
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(200, 0);
     glutInitWindowSize(500, 500);
 
